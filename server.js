3fd7960286ba591e94f21850c431c23d1bf41cdc
Aom <oommankung@gmail.com>
2023-02-09 13:28:35 +0700

    Connect with mongoDB atlas

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const hospitals = require('./routes/hospitals');
+const connectDB = require('./config/db');
 
+//load env vars
 dotenv.config({path:'./config/config.env'});
+
+//connect to database
+connectDB();
 
 const app = express();
 
-const hospitals = require('./routes/hospitals');
-app.use('./routes/hospitals', hospitals);
+//body parser
+app.use(express.json());
+app.use('/api/v1/hospitals', hospitals);
 
 const PORT = process.env.PORT || 5000 ;
 
-app.listen(PORT, console.log('server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log('Error: '+err.message);
+    server.close(()=>process.exit(1));
+})
