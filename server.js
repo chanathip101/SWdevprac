5e1ae8979a35ca00eeb283f7ec87fa59c8c47bd6
Aom <oommankung@gmail.com>
2023-02-22 00:39:36 +0700

    add authentication

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const hospitals = require('./routes/hospitals');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //load env vars
@@ -13,7 +13,16 @@ const app = express();
 
 //body parser
 app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
+
+//route files
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000 ;
 
