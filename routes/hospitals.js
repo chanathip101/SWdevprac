83507ab1e9067527028cd228db80520a858b54a9
Aom <oommankung@gmail.com>
2023-03-15 12:23:45 +0700

    create appt endpoint and add relationship

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals')
 
+// include other resource routers
+const appointmentRouter = require('./appointments');
+
 const router = express.Router() ;
 
 const { protect, authorize } = require('../middleware/auth');
+
+// re-route into other resource routers
+router.use('/:hospitalId/appointments/', appointmentRouter);
 
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect, updateHospital).delete(protect, authorize('admin'), deleteHospital);
--- server.js
+++ server.js
@@ -19,9 +19,12 @@ app.use(cookieParser());
 
 //route files
 const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
 const auth = require('./routes/auth');
 
+//mount routers
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/appointments', appointments);
 app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000 ;
